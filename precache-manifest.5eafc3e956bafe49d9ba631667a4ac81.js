2adb1cdec1ce6e38cff528f56d8158309c6a6054
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-30 16:14:20 +0300

    Updates

---
+++ precache-manifest.5eafc3e956bafe49d9ba631667a4ac81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1290685b139e35ddf0b772737c99b91",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cefeb0e7214493c82b2c",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "94c5e59ee0edf2d0f42c",
+    "url": "./static/css/main.72c8f3e7.chunk.css"
+  },
+  {
+    "revision": "cefeb0e7214493c82b2c",
+    "url": "./static/js/2.51a060dc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.51a060dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c5e59ee0edf2d0f42c",
+    "url": "./static/js/main.8f456a4e.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
